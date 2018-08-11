b6d636f8c1872925a0f073a40b6c0b2a51004592
ParthGhumatkar <36190438+ParthGhumatkar@users.noreply.github.com>
2018-08-11 11:46:01 +0530

    Initial commit

---
+++ Rosettes and Spirals.py
@@ -0,0 +1,31 @@
+import turtle
+t=turtle.Pen()
+t.penup()
+t.speed(0)
+turtle.bgcolor('black')
+sides = int(turtle.numinput("Number of sides"
+                            "how many sides in your spiral?", 4,2,6))
+colors=["red","yellow","blue","green","purple","orange"]
+for m in range(1,60):
+    t.forward(m*4)
+    t.width(m//25+1)
+    position = t.position()
+    heading = t.heading()
+    if (m % 2 == 0):
+        for n in range(sides):
+            t.pendown()
+            t.pencolor(colors[n%sides])
+            t.circle(m/4)
+            t.right(360/sides - 2)
+            t.penup()
+    else:
+        for n in range(3,m):
+            t.pendown()
+            t.pencolor(colors[n%sides])
+            t.forward(n*2/3)
+            t.right(360/sides - 2)
+            t.penup()
+            t.setpos(position)
+            t.setheading(heading)
+            t.left(360/sides + 2)
+    
